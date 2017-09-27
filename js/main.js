f270dc9a1b5d713a59a3e4e5c42cffac8a1de7fe
matrixisolutions <31127867+matrixisolutions@users.noreply.github.com>
2017-09-27 15:43:37 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@
 	var cfg = {		
 		defAnimation   : "fadeInUp",    // default css animation		
 		scrollDuration : 800,           // smoothscroll duration
-		mailChimpURL   : 'https://matrixisolutions.us16.list-manage.com/subscribe/post?u=cdb7b577e41181934ed6a6a44&amp;id=e65110b38d'
+		mailChimpURL   : 'http://www.matrixisolutions.us16.list-manage.com/subscribe/post?u=e2bbab81e6708e87aba63ca1a&amp;id=f25cfdf99e'
 	},	
 
 	$WIN = $(window);
